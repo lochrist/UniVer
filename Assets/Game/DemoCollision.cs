05f68729cc7a0dcf8fce238b1e9a21e80be569cf
Sebastien Phaneuf <lochrist@yahoo.com>
2018-06-10 16:29:40 -0400

    Unity example

--- Assets/Game/DemoCollision.cs
+++ Assets/Game/DemoCollision.cs
@@ -53,57 +53,6 @@ namespace UniVer
 
         #region Demos
         [Demo]
-        public void SingleRectFalling(World world)
-        {
-            // world.CreateRectangle(0, 2, 1, 2, 1);
-            world.CreateRectangle(0, 0, 10, 10, 1);
-        }
-
-        [Demo]
-        public void RectOnFloor(World world)
-        {
-            // world.CreateRectangle(0, 2, 1, 2, 1);
-            world.CreateRectangle(100, 189, 10, 10, 1);
-        }
-
-        [Demo]
-        public void TwoBodies(World world)
-        {
-            world.CreateRectangle(50, 170, 4, 4);
-
-            world.CreateRectangle(40, 180, 32, 4);
-        }
-
-        [Demo]
-        public void FourRectsColliding(World world)
-        {
-            var s = 4.57f;
-            world.CreateRectangle(77f, 74f, s, s, 1);
-            world.CreateRectangle(77f, 80f, s, s, 1);
-
-            world.CreateRectangle(117f, 85, s, s, 1);
-            world.CreateRectangle(117f, 91f, s, s, 1);
-        }
-
-        [Demo]
-        public void TriangleCollision(World world)
-        {
-            var w = world.width / 35f;
-            var h = world.height / 2 - 4.5f * w;
-            world.CreateTriangle(w * 7.5f, h + w * 11, w * 10, w * 2, 20);
-
-            var triangleHeight = world.height - w * 1.8f + 0.4f * w * 0.5f;
-            var triSize = w * 4;
-            world.CreateTriangle(
-                world.width / 4,
-                triangleHeight,
-                triSize,
-                triSize,
-                10
-            );
-        }
-
-        [Demo(true)]
         public void CodePenDemo(World world)
         {
             var codepen = new[]
@@ -118,13 +67,35 @@ namespace UniVer
                 "                    *             ",
                 "                    *             "
             };
+            CreateTextAndSquaresDemo(codepen);
+        }
 
+        [Demo(true)]
+        public void CodeUnityDemo(World world)
+        {
+            var codepen = new[]
+            {
+                "             *                       ",
+                "                                     ",
+                "*  *  *   *  *  *****  *   *  ****   ",
+                "*  *  **  *  *    *     * *   *  *   ",
+                "*  *  * * *  *    *      *    *  *   ",
+                "*  *  *  **  *    *      *    *  *   ",
+                "****  *   *  *    *      *    ****   ",
+                "                                     ",
+                "                                     "
+            };
+            CreateTextAndSquaresDemo(codepen);
+        }
+
+        private void CreateTextAndSquaresDemo(string[] demoText)
+        {
             var w = world.width / 35f;
             var h = world.height / 2 - 4.5f * w;
             var rectSize = w * 0.8f;
-            for (var i = 0; i < codepen.Length; i++)
+            for (var i = 0; i < demoText.Length; i++)
             {
-                var line = codepen[i];
+                var line = demoText[i];
                 for (var j = 0; j < line.Length; j++)
                 {
                     var c = line[j];
@@ -146,6 +117,54 @@ namespace UniVer
                 10
             );
 
+            world.CreateTriangle(
+                world.width / 4,
+                triangleHeight,
+                triSize,
+                triSize,
+                10
+            );
+        }
+        #endregion
+
+        #region Testing Demos
+        public void SingleRectFalling(World world)
+        {
+            // world.CreateRectangle(0, 2, 1, 2, 1);
+            world.CreateRectangle(0, 0, 10, 10, 1);
+        }
+
+        public void RectOnFloor(World world)
+        {
+            // world.CreateRectangle(0, 2, 1, 2, 1);
+            world.CreateRectangle(100, 189, 10, 10, 1);
+        }
+
+        public void TwoBodies(World world)
+        {
+            world.CreateRectangle(50, 170, 4, 4);
+
+            world.CreateRectangle(40, 180, 32, 4);
+        }
+
+        public void FourRectsColliding(World world)
+        {
+            var s = 4.57f;
+            world.CreateRectangle(77f, 74f, s, s, 1);
+            world.CreateRectangle(77f, 80f, s, s, 1);
+
+            world.CreateRectangle(117f, 85, s, s, 1);
+            world.CreateRectangle(117f, 91f, s, s, 1);
+        }
+
+        public void TriangleCollision(World world)
+        {
+            var w = world.width / 35f;
+            var h = world.height / 2 - 4.5f * w;
+            world.CreateTriangle(w * 7.5f, h + w * 11, w * 10, w * 2, 20);
+
+            var triangleHeight = world.height - w * 1.8f + 0.4f * w * 0.5f;
+            var triSize = w * 4;
             world.CreateTriangle(
                 world.width / 4,
                 triangleHeight,
